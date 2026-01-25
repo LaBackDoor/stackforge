0578b8f2fae40974b33398963d26277214983cdc
Abanisenioluwa Kolawole Orojo <b.korojo@gmail.com>
2026-01-25 10:30:07 -0600

    maturin
    maturin update

--- python/stackforge/__init__.py
+++ python/stackforge/__init__.py
@@ -1,1 +1,6 @@
 __version__ = "0.1.1"
+
+# Re-export all public classes from the Rust extension
+from stackforge.stackforge import LayerIndex, LayerKind, Packet
+
+__all__ = ["Packet", "LayerKind", "LayerIndex", "__version__"]
